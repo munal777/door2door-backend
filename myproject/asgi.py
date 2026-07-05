354da2c5157b58a500c41b01525f0e522c192494
munal777 <munalpoudel3@gmail.com>
2026-07-06 00:00:21 +0545

    feat: update the asgi config code structure

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -3,12 +3,12 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from myproject.routing import websocket_urlpatterns
-from myproject.websocket_auth import JWTAuthMiddlewareStack
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 django_asgi_app = get_asgi_application()
+
+from myproject.routing import websocket_urlpatterns
+from myproject.websocket_auth import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
 	{
